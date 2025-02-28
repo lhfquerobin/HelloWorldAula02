be5280ae16ccaa20cfe8410c9ff2640378419cb3
Luiz Henrique F. Querobin <lhfquerobin@minha.fag.edu.br>
2025-02-27 21:23:49 -0300

    Revert "second commit pl"

--- Hello.java
+++ Hello.java
@@ -1,10 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        ola();
-    }
-
-    private static void ola() {
-        System.out.println("Hello World");
     }
 }
